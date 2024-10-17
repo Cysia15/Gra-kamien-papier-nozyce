b61126c92c4eb79ad8de5c5c6850e956d78ccf1c
M.B 2P <mixu12anaix8@gmail.com>
2024-10-17 12:23:12 +0200

    nauczylismy sie case/switch

--- gra/Program.cs
+++ gra/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Gra
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int wybor_pc = rnd.Next(3);
+       
+        Console.WriteLine("Witaj w grze Kamien,papier,nozyce! Z mierz sie z naszym komputerem");
+        Console.WriteLine("wybierz: kamien (k), papier(p), nozyce(n)");
+        string wybor_gracz = Console.ReadLine();
+
+
+        int wybor_gracz_int;
+        switch (wybor_gracz)
+        {
+            case "k":
+                {
+                    wybor_gracz_int = 0;
+                    break;
+                }
+            case "p":
+                {
+                    wybor_gracz_int = 1;
+                    break;
+                }
+            case "n":
+                {
+                    wybor_gracz_int = 2;
+                    break;
+                }
+            default:
+                Console.WriteLine("zły wybór");
+                break;
+        }
+    }
+}
