318fb7cfdf4b9826555215bb3311d2faf7a80905
Cysia15 <mixu12anaix8@gmail.com>
2024-10-23 12:55:58 +0200

    dodalismy rundy,punktacje,i goto

--- gra/Program.cs
+++ gra/Program.cs
@@ -2,35 +2,73 @@
 {
     static void Main(string[] args)
     {
-        Random rnd = new Random();
-        int wybor_pc = rnd.Next(3);
-       
+        int pktgracz = 0;
+        int pktpc = 0;
         Console.WriteLine("Witaj w grze Kamien,papier,nozyce! Z mierz sie z naszym komputerem");
-        Console.WriteLine("wybierz: kamien (k), papier(p), nozyce(n)");
-        string wybor_gracz = Console.ReadLine();
+
+        Console.WriteLine("Wybierz ile rund chcesz zagrac");
+        int wymaganpkt = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("wybrales " + wymaganpkt + "rund");
 
 
-        int wybor_gracz_int;
-        switch (wybor_gracz)
+        while ((pktgracz < wymaganpkt)|| (pktpc < wymaganpkt))
         {
-            case "k":
-                {
-                    wybor_gracz_int = 0;
+Powrot:
+            Random rnd = new Random();
+            int wybor_pc = rnd.Next(3);
+
+
+            Console.WriteLine("wybierz: kamien (k), papier(p), nozyce(n)");
+            string wybor_gracz = Console.ReadLine();
+
+
+            int wybor_gracz_int = 4;
+            switch (wybor_gracz)
+            {
+                case "k":
+                    {
+                        wybor_gracz_int = 0;
+                        break;
+                    }
+                case "p":
+                    {
+                        wybor_gracz_int = 1;
+                        break;
+                    }
+                case "n":
+                    {
+                        wybor_gracz_int = 2;
+                        break;
+                    }
+                default:
+                    Console.WriteLine("zły wybór");
+                    goto Powrot;
                     break;
-                }
-            case "p":
-                {
-                    wybor_gracz_int = 1;
-                    break;
-                }
-            case "n":
-                {
-                    wybor_gracz_int = 2;
-                    break;
-                }
-            default:
-                Console.WriteLine("zły wybór");
-                break;
+            }
+            if (wybor_gracz_int == wybor_pc)
+            {
+                Console.WriteLine("remisik");
+                
+                goto Powrot;
+               
+                
+            }
+            else if ((wybor_gracz_int == 0 && wybor_pc == 1) || (wybor_gracz_int == 2 && wybor_pc == 0))
+            {
+                Console.WriteLine("komputerix wygrywa ");
+                pktpc++;
+                Console.WriteLine("Gracz: " + pktgracz + " ------" + " komputer: " + pktpc);
+                goto Powrot;
+            }
+            else if ((wybor_gracz_int == 0 && wybor_pc == 2) || (wybor_gracz_int == 1 && wybor_pc == 0))
+            {
+                Console.WriteLine("ZIOOOM ALE WINEK");
+                pktgracz++;
+                Console.WriteLine($"Gracz: {pktgracz}   ------  komputer: {pktpc}");
+                goto Powrot;
+            }
         }
+        if
     }
 }
